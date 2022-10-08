33a86f2d571de986d96eef4bbf93e818a30c11c3
Айси <IceIX@MacBook-Pro-Filipp.local>
2022-10-08 14:05:06 +0300

    zadacha2

---
+++ zadacha2/Program.cs
@@ -0,0 +1,15 @@
+﻿/* Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+2, 3, 7 -> 7
+44 5 78 -> 78
+22 3 9 -> 22 */
+
+Console.WriteLine("Введите три числа:");
+int a = int.Parse(Console.ReadLine());
+int b = int.Parse(Console.ReadLine());
+int c = int.Parse(Console.ReadLine());
+
+int max = Math.Max(Math.Max(a, b), c);
+
+Console.WriteLine("{0} - это наибольшее число", max);
+
